5d1b2c94ddc8a3b8ac2e65b0ef82414e30fedf06
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-02-20 11:34:23 +0100

    implement, switch,redirect for login to the user page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
+import Form from './components/Form';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {userName: '', password: '', hasFailed: false, isAuth: false}
+    this.changeFormField = this.changeFormField.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  changeFormField(event) {
+    switch(event.target.getAttribute('ident')) {
+      case 'usr':
+        this.setState({userName: event.target.value});
+        break;
+      case 'pwd':
+        this.setState({password: event.target.value});
+        break;
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.userName === 'Jake' && this.state.password === 'djarte023') {
+      console.log('You are logged in!');
+      this.setState({hasFailed: false, isAuth: true});
+    } else {
+      this.setState({hasFailed: true});
+    }
+  }
+
   render() {
+    const formProps = {
+      userName: this.state.userName,
+      password: this.state.password,
+      handleChange: this.changeFormField,
+      handleSubmit:this.handleSubmit,
+      hasFailed: this.state.hasFailed
+    }
+
     return (
-      <h2>Hello react</h2>
+      <BrowserRouter>
+        <>
+            <Route exact path="/" render={() => <Form formProps={ formProps } />} />
+
+            <Switch>
+              <Route path="/user" render={() => <h3>Hello User</h3>} />
+              {this.state.isAuth && <Redirect to="/user" />}
+            </Switch>
+        </>
+      </BrowserRouter>
     )
   }
 }
