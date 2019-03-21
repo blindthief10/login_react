1327b3703b915604162105622991eb8706144d15
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-03-21 16:10:01 +0100

    added build folder

---
+++ build/precache-manifest.804f1a8e048c5d75b09a2956eef63088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "12ca5dad560671816b35",
+    "url": "/static/js/main.12ca5dad.chunk.js"
+  },
+  {
+    "revision": "c3fd8e3d1f1494006406",
+    "url": "/static/js/2.c3fd8e3d.chunk.js"
+  },
+  {
+    "revision": "c3fd8e3d1f1494006406",
+    "url": "/static/css/2.82f4af24.chunk.css"
+  },
+  {
+    "revision": "0f813a7bd0a50b7548d280251a1e6177",
+    "url": "/index.html"
+  }
+];
